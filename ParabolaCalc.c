b0a20307819c8fef0b449723e82f3a7e54e69630
hgeen <48893515+hgeen@users.noreply.github.com>
2023-06-03 18:12:51 -0400

    first commit
    actually uploaded the c file because, duhh, you need the file to actually run the calculator

---
+++ ParabolaCalc.c
@@ -0,0 +1,107 @@
+#include <math.h>
+#include <stdio.h>
+
+int parabolaTable() 
+{
+    float a;
+    float b;
+    float c;
+    int x;
+    int end;
+    float y;
+
+
+    printf("please enter a value for a\n");
+    scanf_s("%f", &a);
+    printf("please enter a value for b\n");
+    scanf_s("%f", &b);
+    printf("please enter a value for c\n");
+    scanf_s("%f", &c);
+    printf("please enter a value for the starting value\n");
+    scanf_s("%d", &x);
+    printf("please enter a value for the ending value\n");
+    scanf_s("%d", &end);
+
+
+    for (int i = x; i <= end; i++)
+    {
+        y = a * (i * i) + (b * i) + c;
+        printf("point %d is (%d, %f)\n", i, i, y);
+    }
+
+    return 0;
+}
+
+int parabolaVertex()
+{
+    float a;
+    float b;
+    float c;
+    float x;
+    float y;
+
+
+    printf("please enter a value for a\n");
+    scanf_s("%f", &a);
+    printf("please enter a value for b\n");
+    scanf_s("%f", &b);
+    printf("please enter a value for c\n");
+    scanf_s("%f", &c);
+
+    x = -b / (2 * a);
+    y = a * (x * x) + (b * x) + c;
+    printf("the vertex is (%f, %f)", x, y);
+
+    return 0;
+}
+
+int parabolaIntercepts()
+{
+    float a;
+    float b;
+    float c;
+    float yIntercept;
+    float radicand;
+    float xInterceptOne;
+    float xInterceptTwo;
+
+
+    printf("please enter a value for a\n");
+    scanf_s("%f", &a);
+    printf("please enter a value for b\n");
+    scanf_s("%f", &b);
+    printf("please enter a value for c\n");
+    scanf_s("%f", &c);
+
+    yIntercept = (a * 0) + (b * 0) + c;
+    radicand = sqrtf((b * b) - (4 * a * c));
+    if (radicand < 0)
+    {
+        return 0;
+    }
+    else if (radicand >= 0)
+    {
+        xInterceptOne = (-b + sqrtf(radicand)) / 2 * a;
+        xInterceptTwo = (-b - sqrtf(radicand)) / 2 * a;
+        printf("x intercept one is %f\n", xInterceptOne);
+        printf("x intercept two is %f\n", xInterceptTwo);
+    }
+    printf("y intercept is %f", yIntercept);
+
+    return 0;
+}
+
+int main()
+{
+    int choice;
+
+    printf("do you need x-y intercepts, a table, or the vertex? 1 for intercepts, 2 for the table, 3 for the vertex\n");
+    scanf_s("%d", &choice);
+    if (choice == 1)
+    {
+        printf("you have chosen x-y intercepts");
+        parabolaIntercepts();
+    }
+
+    return 0;
+}
