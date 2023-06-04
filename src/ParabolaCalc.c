3df4413429814129f07b574ec684018e2ceb08fc
hgeen <48893515+hgeen@users.noreply.github.com>
2023-06-03 20:42:27 -0400

    tiny main fix
    made it so that if the choice integer isn't zero, the main function will call itself and make the user enter an accepted input

--- src/ParabolaCalc.c
+++ src/ParabolaCalc.c
@@ -10,7 +10,7 @@ int parabolaTable()
     int end;
     float y;
 
-
+    // should probably add a system that prevents the user from not entering in a number
     printf("please enter a value for a\n");
     scanf_s("%f", &a);
     printf("please enter a value for b\n");
@@ -22,7 +22,7 @@ int parabolaTable()
     printf("please enter a value for the ending value\n");
     scanf_s("%d", &end);
 
-
+    // the for loop that does it all, this baby calculates the quadratic formula which is x = (-b ± √(b² - 4ac)) / 2a
     for (int i = x; i <= end; i++)
     {
         y = a * (i * i) + (b * i) + c;
@@ -48,6 +48,7 @@ int parabolaVertex()
     printf("please enter a value for c\n");
     scanf_s("%f", &c);
 
+    // says that the x value of the vertex is -b / 2a
     x = -b / (2 * a);
     y = a * (x * x) + (b * x) + c;
     printf("the vertex is (%f, %f)", x, y);
@@ -73,6 +74,8 @@ int parabolaIntercepts()
     printf("please enter a value for c\n");
     scanf_s("%f", &c);
 
+    // y intercept is stated to be a quadratic expression
+    // the radicand is the inside of the radical of the quadratic equation, which is the square root of b squared minus four times a times c or √(b² - 4ac)
     yIntercept = (a * 0) + (b * 0) + c;
     radicand = sqrtf((b * b) - (4 * a * c));
     if (radicand < 0)
@@ -82,6 +85,7 @@ int parabolaIntercepts()
     }
     else if (radicand >= 0)
     {
+        // does the quadratic equation with both plus and minus parts
         xInterceptOne = (-b + sqrtf(radicand)) / 2 * a;
         xInterceptTwo = (-b - sqrtf(radicand)) / 2 * a;
         printf("x intercept one is %f\n", xInterceptOne);
@@ -96,8 +100,11 @@ int main()
 {
     int choice;
 
+    // gets input to choose which function
     printf("do you need x-y intercepts, a table, or the vertex? 1 for intercepts, 2 for the table of points, 3 for the vertex\n");
     scanf_s("%d", &choice);
+
+    // if statement mess, switch statements wont work
     if (choice == 1)
     {
         printf("you have chosen x-y intercepts\n");
@@ -112,6 +119,11 @@ int main()
     {
         printf("you have chosen the vertex\n");
         parabolaVertex();
+    }
+    else if(choice != 3 && choice != 2 && choice != 1)
+    {
+        printf("that is not a valid number, please try again\n");
+        main();
     }
 
     return 0;
